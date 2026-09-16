35206d2a3cb16db71e4a8cd5be1f717b9469a530
Matheus C. França <matheus-catarino@hotmail.com>
2026-09-16 16:37:39 -0300

    GGML changes

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -238,7 +238,7 @@
 // Maximum number of model contexts (e.g., for model shards).
 // Increase this value using -DGGML_MAX_CONTEXTS=<value> in CMake
 // if you need to load more than 64 model shards.
-#define GGML_MAX_CONTEXTS       64
+#define GGML_MAX_CONTEXTS       2048
 #endif
 #ifndef GGML_MAX_SRC
 // For the machines with 11+ GPUs use -DGGML_MAX_SRC=N
